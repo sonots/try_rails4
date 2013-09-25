3f4200f30da17457346dd64010a5d57c4aaf7ce0
sonots <sonots@gmail.com>
2013-09-25 20:56:04 +0900

    rails new try_rails4 --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TryRails4::Application.config.secret_key_base = '7889bdd8f279fb1cad00e1657ab2116ad07595e39256dad3d0c3be8b1b585dbbfca0c59d43737ec61f9d993e26bb6395d40ad079e090308b041f2a170bd5fb97'
